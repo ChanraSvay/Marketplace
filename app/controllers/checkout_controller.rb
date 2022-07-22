e86ede1e521175fea7df2d3d3c5bf340af72e1f9
chiisvay <98688318+chiisvay@users.noreply.github.com>
2022-07-23 00:41:52 +1000

    changes to stripe.rb config

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,18 +1,21 @@
 class CheckoutController < ApplicationController
 
     def create  
-        product = Product.find(params[:id])
+      @product = Product.find(params[:id])
       @session = Stripe::Checkout::Session.create({
   payment_method_types: ['card'],
   line_items: [{
-    amount: product.price,
+    amount: @product.price,
     currency: "aud",
-    quantity: 1
+    quantity: 1,
+    name: @product.id
   }],
   mode: 'payment',
   success_url: root_url,
   cancel_url: root_url,
 })
-    end 
-
+    respond_to do |format|
+        format.json
+    end
+   end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
 Rails.configuration.stripe = {
-    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :public_key => ENV['PUBLIC_KEY'],
     :secret_key => ENV['SECRET_KEY']
 
 }
-Stripe.api_key = Rails.application.credentials[:stripe][:secret_key]
+# Stripe.api_key = Rails.application.credentials[:stripe][:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.application.credentials.stripe_secret_key
